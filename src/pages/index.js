d4f3ae0abc1e3672e55240ede180f8a20ff6540f
Dave Iverson <dave.iverson@raizlabs.com>
2019-09-03 15:27:28 -0500

    Added contentful content for demo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,30 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Clock from "../components/clock";
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>Meta Description: {data.site.siteMetadata.description}</p>
+    <p>Total MCs: {data.allContentfulMc.totalCount}</p>
+    <ol>
+      {data.allContentfulMc.nodes.map(mc => {
+        return <li>
+        <h2>{mc.name}</h2>
+        <p>
+          {documentToReactComponents(mc.bio.json)}
+        </p>
+        </li>
+      })}
+    </ol>
     <Clock />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
@@ -19,5 +32,32 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query IndexPageQuery {
+    site {
+      siteMetadata {
+        description
+      }
+    }
+    allContentfulMc {
+      nodes {
+        updatedAt
+        name
+        id
+        createdAt
+        contentful_id
+        location {
+          lat
+          lon
+        }
+        bio {
+          json
+        }
+      }
+      totalCount
+    }
+  }
+`
 
 export default IndexPage
